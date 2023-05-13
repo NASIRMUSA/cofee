6810384bf28edfaee3e8f627c306f64442f60696
NASIRMUSA <31094186+NASIRMUSA@users.noreply.github.com>
2023-05-13 04:29:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var conta = document.getElementById('conta');
+ 
+// conta.style.display = "none";
+
+function container() {
+	// body...
+	var name = document.getElementById('new').value;
+	var Num_bar = document.QuenrySelector('new');
+
+	for (var i = new.length - 1; i >= 0; i++) {
+		Num_bar.length=20;
+		name.value+Num_bar;
+		new[i]
+	}
+}
